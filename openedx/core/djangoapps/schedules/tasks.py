442cc1558728997c074e062eca503cfb5d299a7c
Gabe Mulley <gabe@edx.org>
2017-09-06 09:33:24 -0400

    actually encode urls

--- openedx/core/djangoapps/schedules/tasks.py
+++ openedx/core/djangoapps/schedules/tasks.py
@@ -1,5 +1,5 @@
 import datetime
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError
 from urlparse import urlparse
 
 from celery.task import task
@@ -81,43 +81,19 @@ def _recurring_nudge_schedules_for_hour(target_hour, org_list, exclude_orgs=Fals
         course_id_str = str(enrollment.course_id)
         course = enrollment.course
 
-        course_root = reverse('course_root', args=[course_id_str])
-        dashboard_url = reverse('dashboard')
-
-        def encode_url(url):
-            # Sailthru has a bug where URLs that contain "+" characters in their path components are misinterpretted
-            # when GA instrumentation is enabled. We need to percent-encode the path segments of all URLs that are
-            # injected into our templates to work around this issue.
-            parsed_url = urlparse(url)
-            parsed_url._replace(path=urlquote(parsed_url.path))
-            return parsed_url.geturl()
-
-        def absolute_url(relative_path):
-            return encode_url(u'{}{}'.format(settings.LMS_ROOT_URL, relative_path))
-
-        def encode_urls_in_dict(mapping):
-            urls = {}
-            for key, value in mapping.iteritems():
-                urls[key] = encode_url(value)
-            return urls
-
-        try:
-            template_revision = check_output(['git', 'log', '-1', '--format=%H'])
-            template_tag = check_output(['git', 'describe', '--always', '--tags'])
-        except Exception:  # pylint: disable=broad-except
-            template_revision = ""
-            template_tag = ""
+        course_root_relative_url = reverse('course_root', args=[course_id_str])
+        dashboard_relative_url = reverse('dashboard')
 
         template_context = {
             'student_name': user.profile.name,
             'course_name': course.display_name,
-            'course_url': absolute_url(course_root),
+            'course_url': absolute_url(course_root_relative_url),
 
             # Platform information
             'homepage_url': encode_url(marketing_link('ROOT')),
-            'dashboard_url': absolute_url(dashboard_url),
-            'template_revision': template_revision,
-            'template_tag': template_tag,
+            'dashboard_url': absolute_url(dashboard_relative_url),
+            'template_revision': get_scm_revision(),
+            'template_tag': get_scm_tag(),
             'platform_name': settings.PLATFORM_NAME,
             'contact_mailing_address': settings.CONTACT_MAILING_ADDRESS,
             'social_media_urls': encode_urls_in_dict(getattr(settings, 'SOCIAL_MEDIA_FOOTER_URLS', {})),
@@ -128,3 +104,37 @@ def _recurring_nudge_schedules_for_hour(target_hour, org_list, exclude_orgs=Fals
         }
 
         yield (user, course.language, template_context)
+
+
+def get_scm_revision():
+    try:
+        return check_output(['git', 'log', '-1', '--format=%H']).strip()
+    except CalledProcessError:
+        return ''
+
+
+def get_scm_tag():
+    try:
+        return check_output(['git', 'describe', '--always', '--tags']).strip()
+    except CalledProcessError:
+        return ''
+
+
+def encode_url(url):
+    # Sailthru has a bug where URLs that contain "+" characters in their path components are misinterpreted
+    # when GA instrumentation is enabled. We need to percent-encode the path segments of all URLs that are
+    # injected into our templates to work around this issue.
+    parsed_url = urlparse(url)
+    modified_url = parsed_url._replace(path=urlquote(parsed_url.path))
+    return modified_url.geturl()
+
+
+def absolute_url(relative_path):
+    return encode_url(u'{}{}'.format(settings.LMS_ROOT_URL, relative_path))
+
+
+def encode_urls_in_dict(mapping):
+    urls = {}
+    for key, value in mapping.iteritems():
+        urls[key] = encode_url(value)
+    return urls
